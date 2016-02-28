6aff7881c3d4504b7ad230b9c21bc795a1a94b57
Pairs Tao <paristao1989@gmail.com>
2016-02-28 11:40:34 +0800

    03 reader interface

--- src/main/java/com/chinesedreamer/toolkit/excel/parse/service/impl/Excel03ReaderServiceImpl.java
+++ src/main/java/com/chinesedreamer/toolkit/excel/parse/service/impl/Excel03ReaderServiceImpl.java
@@ -1,8 +1,14 @@
 package com.chinesedreamer.toolkit.excel.parse.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.chinesedreamer.toolkit.excel.parse.entity.Excel;
 import com.chinesedreamer.toolkit.excel.parse.service.ExcelReaderService;
 import com.chinesedreamer.toolkit.excel.schema.bean.AbstractExcelProcesserConfiguration;
+import com.chinesedreamer.toolkit.excel.schema.bean.ExcelProcesserConfiguration;
+import com.chinesedreamer.toolkit.excel.schema.bean.ExcelTitleConfiguration;
 
 /** 
  * Description: 
@@ -16,8 +22,41 @@ public class Excel03ReaderServiceImpl implements ExcelReaderService{
 	@Override
 	public Object readExcel(Excel excel,
 			AbstractExcelProcesserConfiguration configuration) {
-		// TODO Auto-generated method stub
+		if (configuration instanceof ExcelProcesserConfiguration) {
+			return this.readExcelProcesserConfiguration(excel, (ExcelProcesserConfiguration)configuration);
+		}
 		return null;
 	}
-
+	
+	/**
+	 * 解析参数类型为ExcelProcesserConfiguration的文件
+	 * @param excel
+	 * @param configuration
+	 * @return
+	 */
+	private Object readExcelProcesserConfiguration(Excel excel,ExcelProcesserConfiguration configuration) {
+		//1. 获取title column对应的property
+		Map<Integer, ExcelTitleConfiguration> titleMap = this.getTitleMap(configuration);
+		//2. 逐行格封装参数 TODO
+		String modelClass = configuration.getTargetEntity();
+		try {
+			Object model = Class.forName(modelClass).newInstance();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block logback
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 获取列对应的property和handler
+	 * @param configuration
+	 * @return
+	 */
+	private Map<Integer, ExcelTitleConfiguration> getTitleMap(ExcelProcesserConfiguration configuration) {
+		Map<Integer, ExcelTitleConfiguration> titleMap = new HashMap<Integer, ExcelTitleConfiguration>();
+		for (ExcelTitleConfiguration titleConfiguration : configuration.getTitleConfigurations()) {
+			titleMap.put(titleConfiguration.getColumn(), titleConfiguration);
+		}
+		return titleMap;
+	}
 }
---
+++ src/main/java/com/chinesedreamer/toolkit/excel/parse/util/ExcelUtil.java
@@ -0,0 +1,11 @@
+package com.chinesedreamer.toolkit.excel.parse.util;
+/** 
+ * Description: 
+ * @author Paris Tao
+ * @version 1.0beta
+ * @date 2016年2月28日 上午11:37:04 
+ * Copyright:   Copyright (c)2015
+ */
+public class ExcelUtil {
+
+}
