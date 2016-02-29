bbf9287e2bb18d54cf41c58ce1db24b8d75ed6d2
Paris Tao <paristao1989@gmail.com>
2016-02-29 17:29:59 +0800

    implement getCellValue method

--- src/main/java/com/chinesedreamer/toolkit/excel/parse/util/ExcelUtil.java
+++ src/main/java/com/chinesedreamer/toolkit/excel/parse/util/ExcelUtil.java
@@ -1,8 +1,12 @@
 package com.chinesedreamer.toolkit.excel.parse.util;
 
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import java.math.BigDecimal;
+
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** 
  * Description: 
@@ -12,6 +16,9 @@ import org.apache.poi.ss.usermodel.Row;
  * Copyright:   Copyright (c)2015
  */
 public class ExcelUtil {
+	
+	private static Logger logger = LoggerFactory.getLogger(ExcelUtil.class);
+	
 	/**
 	 * 判断row是否是空行
 	 * @param row
@@ -36,7 +43,56 @@ public class ExcelUtil {
 		if (cell == null) {
 			return null;
 		}
-		cell.getCellStyle().equals(HSSFCellStyle.)
-		return null;
+		Object value = null;
+		switch (cell.getCellType()) {
+		case Cell.CELL_TYPE_BLANK:
+			break;
+		case Cell.CELL_TYPE_BOOLEAN:
+			if (clazz.getName().equals("java.lang.Boolean")) {
+				value = cell.getBooleanCellValue();
+			}else {
+				logger.error("Can't convert boolean value to Boolean property.");
+			}
+			break;
+		case Cell.CELL_TYPE_ERROR:
+			break;
+		case Cell.CELL_TYPE_FORMULA:
+			value = convertValue2TargetType(clazz, cell.getCellFormula());
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			if (HSSFDateUtil.isCellDateFormatted(cell)){
+				value = cell.getDateCellValue();
+			}else {
+				value = convertValue2TargetType(clazz, String.valueOf(cell.getNumericCellValue()));
+			}
+			break;
+		case Cell.CELL_TYPE_STRING:
+			value = convertValue2TargetType(clazz, cell.getStringCellValue());
+			break;
+		default:
+			break;
+		}
+		return value;
+	}
+	
+	private static Object convertValue2TargetType(Class<?> clazz, String tempValue) {
+		if (clazz.getName().equals("java.lang.Boolean")) {
+			return tempValue;
+		}else if (clazz.getName().equals("java.lang.Integer")) {
+			return Integer.valueOf(tempValue);
+		}else if (clazz.getName().equals("java.lang.Float")) {
+			return Float.valueOf(tempValue);
+		}else if (clazz.getName().equals("java.lang.Double")) {
+			return Double.valueOf(tempValue);
+		}else if (clazz.getName().equals("java.lang.Short")) {
+			return Short.valueOf(tempValue);
+		}else if (clazz.getName().equals("java.lang.Long")) {
+			return Long.valueOf(tempValue);
+		}else if (clazz.getName().equals("java.lang.BigDecimal")) {
+			return new BigDecimal(tempValue);
+		}else {
+			logger.error("Not supported type of {} for formula cell.", clazz.getName());
+			return null;
+		}
 	}
 }
